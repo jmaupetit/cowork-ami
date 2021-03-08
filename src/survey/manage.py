1848147c8b94c8ba3d7860829059944beb02a3fd
Julien Maupetit <julien@maupetit.net>
2021-03-08 21:09:14 +0100

    Rename project to survey

--- src/survey/manage.py
+++ src/survey/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cowork.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "survey.settings")
     try:
         # pylint: disable=import-outside-toplevel
         from django.core.management import execute_from_command_line
--- src/survey/survey/wsgi.py
+++ src/survey/survey/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for cowork project.
+WSGI config for survey project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cowork.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "survey.settings")
 
 application = get_wsgi_application()
